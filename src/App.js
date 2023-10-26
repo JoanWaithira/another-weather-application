108726f0fa4c90319a1136d76548de3f1cb0105e
Joan Waithira Njoroge <joanwaithira.jw@gmail.com>
2023-10-26 14:25:46 +0300

    Making a few changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import Search from "./Search";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1> Hello World</h1>
+//       </header>
+
+//       <Search />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello World</h1>
-      </header>
-
+      <h1> Weather Application </h1>
       <Search />
     </div>
   );
 }
-
-export default App;
