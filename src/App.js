b368c9ae2056501ebfeedd1bd48d535114177b98
Joan Waithira Njoroge <joanwaithira.jw@gmail.com>
2023-10-26 15:09:24 +0300

    Push this

--- src/App.js
+++ src/App.js
@@ -3,25 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Search from "./Search";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1> Hello World</h1>
-//       </header>
-
-//       <Search />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1> Weather Application </h1>
+      <header className="App-header">
+        <h1> Hello World</h1>
+      </header>
+
       <Search />
     </div>
   );
 }
+
+export default App;
